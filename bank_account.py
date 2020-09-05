c985e1bf5e49929079f7bbc4379b40cb2448db5c
dylanPMurphy <dylanmurphydba@gmail.com>
2020-09-04 20:05:14 -0700

    init with chaining

---
+++ bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, int_rate, balance):
+        pass
+
+    def deposit(self, amnt):
+        return  self
+
+    def withdraw(self, amnt):
+        return  self
+
+    def displayAccontInfo(self):
+        return  self
+
+    def yeildInterest(self):
+        return self
